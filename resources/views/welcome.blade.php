beac04ff0a6594402c24118d4b51b8e3b5f2af5b
Sylvestre AHLONSOU <sylvestrea863@gmail.com>
2023-02-18 14:02:33 +0100

    Mon premier commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,24 +8,24 @@
     <div class="row">
         <p class="col-md-6">
             Crud Laravel Application est  une application web (base de donnée)
-            conçue dans le but de maitriser les bases du framework php 
-            <mark>Laravel</mark> .
+            conçue dans le but de maitriser les bases du framework php
+            <mark>Laravel.</mark> .
         </p>
         <p class="col-md-6">
         <mark>CRUD</mark>est un acronyme des noms des quatre opérations de base de la gestion de la persistance
-         des données et applications 
+         des données et applications
         : Create (créer) Read ou Retrieve (lire) Update (mettre à jour) Delete ou Destroy (supprimer)
-            
+
         </p>
         <p class="col-md-6">
-           Crud Laravel Application est une application web conçue avec le framework php <mark>Laravel</mark> et le framework css <mark>bootsrap</mark> 
+           Crud Laravel Application est une application web conçue avec le framework php <mark>Laravel</mark> et le framework css <mark>bootsrap</mark>
         </p>
         <p class="col-md-6">
             Crud Laravel Application est  une application web (base de donnée)
-            conçue dans le but de maitriser les bases du framework php 
+            conçue dans le but de maitriser les bases du framework php
             <mark>Laravel</mark> .
         </p>
     </div>
-    
+
 </div>
 @endsection
